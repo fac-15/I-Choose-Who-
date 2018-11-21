73f5fc6fb6589c977e2242515b008b91eb6d3b37
aterna01 <ssaniok90@mail.ru>
2018-11-21 14:43:55 +0000

    handler version 1

--- src/handlers.js
+++ src/handlers.js
@@ -1,1 +1,20 @@
+const fs = require("fs");
+const path = require("path");
 
+const handleHomeRoute = (request, response) => {
+  const filePath = path.join(__dirname, "..", "public", "index.html");
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      response.writeHead(500, "Content-Type: text/html");
+      response.end("<h1>Sorry, we've had a problem on our end</h1>");
+    } else {
+      response.writeHead(200, "Content-Type: text/html");
+      response.end(file);
+    }
+  });
+};
+
+module.exports = {
+  handleHomeRoute
+};
--- src/router.js
+++ src/router.js
@@ -1,1 +1,17 @@
+// Require the module you have created, i.e. handlers.js, at the top of your router.js
 
+const handlers = require("./handlers.js");
+
+const router = (request, response) => {
+  const url = request.url;
+  if (url === "/") {
+    handlers.handleHomeRoute(request, response);
+  } else if (url.indexOf("public") !== -1) {
+    handlers.handlePublic(request, response, url);
+  } else {
+    response.writeHead(404, "Content-Type: text/html");
+    response.end("<h1>404 file not found</h1>");
+  }
+};
+
+module.exports = router;
