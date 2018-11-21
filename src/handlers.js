6429a14df342a2ccd420a9c9f25ce623775e1280
jcharnley <jasoncharnley@gmail.com>
2018-11-21 15:31:51 +0000

    jc newest

--- src/handlers.js
+++ src/handlers.js
@@ -16,21 +16,32 @@ const handleHomeRoute = (request, response) => {
 };
 
 const handlePublic = (request, response, url) => {
-  const extension = url.split('.')[1];
+  const extension = url.split(".")[1];
   const extensionType = {
-    html: 'text/html',
-    css: 'text/css',
-    js: 'application/javascript',
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
     //ico: 'image/x-icon',
-    jpg: 'image/jpeg',
-    png: 'image/png'
+    jpg: "image/jpeg",
+    png: "image/png"
   };
 
-
-}
-
-
+  const filePath = path.join(__dirname, "..", url);
+  console.log(filePath);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      response.writeHead(404, "Content-Type: text/html");
+      response.end("<h1>404 file not found</h1>");
+    } else {
+      response.writeHead(200, `Content-Type: ${extensionType[extension]}`);
+      response.end(file);
+    }
+  });
+  console.log(url);
+};
 
 module.exports = {
-  handleHomeRoute
+  handleHomeRoute,
+  handlePublic
 };
